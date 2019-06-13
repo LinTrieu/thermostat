013168b362b2c433fb22958ea18983288b83a5b9
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-13 12:01:49 +0100

    Adds API get for OpenWeather temperature

--- src/thermostatController.js
+++ src/thermostatController.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 
   $('#temperature-reset').on('click', function(){
     thermostat.reset();
-    $('#temp-display').text(`Temperature: ${thermostat.temp} degrees`);
+    $('#temp-display').text(`Thermostat temperature: ${thermostat.temp} degrees`);
   });
 
   $('#powersaving-switch').on('click', function() {
@@ -25,7 +25,8 @@ $(document).ready(function(){
     $('#power-saving-status').text(`Power saving status: ${thermostat.showPowerSaving()}`);
   });
 
-
-
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=f6f7691c4b4f18e7bf3e92c2a1334090',function(data) {
+    $('#API-temp-display').text(`OpenWeatherMap API temperature: ${data.main.temp} degrees`);
+    });
 
 });
